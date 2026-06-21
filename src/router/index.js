254582dbe62ad1cf06b7dc8c9ad8e6662c23caa8
pedrohenriqueque <108901318+pedrohenriqueque@users.noreply.github.com>
2026-06-21 15:36:21 -0300

    feat: adicionando tela da carteira

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import MercadoView from '../views/MercadoView.vue'
 import ContaCorrenteView from '../views/ContaCorrenteView.vue'
+import CarteiraView from '../views/CarteiraView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/conta-corrente',
       name: 'conta-corrente',
       component: ContaCorrenteView
+    },
+    {
+      path: '/carteira',
+      name: 'carteira',
+      component: CarteiraView
     }
   ]
 })
